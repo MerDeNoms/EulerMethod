aed0a865321be841383166e9e4e046629777376c
MerDeNoms <neon.cloud@protonmail.com>
2019-05-28 13:42:41 +0300

    time_interval added

--- EulerMethod_Solver/main.py
+++ EulerMethod_Solver/main.py
@@ -13,7 +13,7 @@ class Main(tk.Frame):
     def init_main(self):
         self.equation_label = tk.Label(text="y''+    y'+    y=    x", font='Arial, 16')
         self.equation_label.place(x=40, y=20)
-        self.time_label = tk.Label(text="T =", font='Arial, 16')
+        self.time_label = tk.Label(text="T =        h = ", font='Arial, 16')
         self.time_label.place(x=40, y=60)
         self.first_var = ttk.Entry(width=1, font='Arial, 14')
         self.first_var.place(x=80, y=22)
@@ -23,12 +23,14 @@ class Main(tk.Frame):
         self.third_var.place(x=178, y=22)
         self.time_var = ttk.Entry(width=2, font='Arial, 14')
         self.time_var.place(x=80, y=60)
+        self.time_interval_var = ttk.Entry(width=4, font='Arial, 14')
+        self.time_interval_var.place(x=155, y=60)
         self.button = tk.Button(root, bg="white", text=u"SOLVE !", command=self.open_graph)
-        self.button.place(x=150, y=63)
+        self.button.place(x=215, y=63)
 
     def open_graph(self):
         a, b, c = int(self.first_var.get()), int(self.second_var.get()), int(self.third_var.get())
-        dt = 0.01
+        dt = float(self.time_interval_var.get())
         time = int(self.time_var.get())
         y0 = 0
         v0 = 0
@@ -54,6 +56,6 @@ if __name__ == "__main__":
     app = Main(root)
     app.pack()
     root.title("EulerMethod")
-    root.geometry("250x100+300+200")
+    root.geometry("270x100+300+200")
     root.resizable(False, False)
     root.mainloop()
